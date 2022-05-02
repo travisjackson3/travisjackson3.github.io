cb1019031086be0fc40c910bd9272cdf66b0f554
travisjackson3 <travisjackson3@gmail.com>
2022-05-02 12:17:26 -0700

    updates

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ Vue.createApp({
     methods: {
         increment() {
             this.count++
+          },
+
+          myFunction(){
+            document.getElementById("myLinks");
+            if (x.style.display === "block") {
+              x.style.display = "none";
+            } else {
+              x.style.display = "block";
+            }
           }
   
     }
